6ddb7272cde482b66b9984c78a65c2c281b847ae
Michael Whapples <mwhapples@aim.com>
2023-04-21 21:42:58 +0100

    Refactor test code.

--- tests/bana/test_page_detector.py
+++ tests/bana/test_page_detector.py
@@ -1,5 +1,3 @@
-import math
-
 from brf2ebrf.bana import PageDetector
 from brf2ebrf.parser import DetectionResult
 
@@ -9,16 +7,17 @@ def test_find_first_page():
     brf = "TE/ BRF\nEXTRA TEXT\f"
     actual = page_detector(brf, 0, "StartBraillePage")
     expected_brf = "\ue000{\"BraillePage\": {}}\ue001" + brf.rstrip("\f")
-    assert actual.text == expected_brf
-    assert actual.cursor == len(brf)
-    assert actual.state == "StartBraillePage"
-    assert math.isclose(actual.confidence, 1.0)
+    expected = DetectionResult(expected_brf, len(brf), "StartBraillePage", 1.0)
+    assert actual == expected
+
 
 def test_assume_all_is_page_when_no_form_feed():
-    brf ="TE/ TEXT\nEXTRA TEXT"
-    expected = DetectionResult(text="\ue000{\"BraillePage\": {}}\ue001" + brf, cursor=len(brf), state="StartBraillePage", confidence=1.0)
+    brf = "TE/ TEXT\nEXTRA TEXT"
+    expected = DetectionResult(text="\ue000{\"BraillePage\": {}}\ue001" + brf, cursor=len(brf),
+                               state="StartBraillePage", confidence=1.0)
     actual = PageDetector()(brf, 0, "StartBraillePage")
     assert actual == expected
+
 
 def test_when_state_does_not_apply():
     expected = DetectionResult("", 0, "OtherState", 0.0)
