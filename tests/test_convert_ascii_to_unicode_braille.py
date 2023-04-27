6a7d4d9b8c6407b757a648d5cdbd920e731ce422
Michael Whapples <mwhapples@aim.com>
2023-04-27 16:03:51 +0100

    More test for ASCII to unicode conversion

--- tests/test_convert_ascii_to_unicode_braille.py
+++ tests/test_convert_ascii_to_unicode_braille.py
@@ -7,7 +7,9 @@ from brf2ebrf.parser import DetectionResult
     ("TEST", 0, "\u281e\u2811\u280e\u281e"),
     ("TEST", 1, "\u2811\u280e\u281e"),
     ("TEST", 2, "\u280e\u281e"),
-    ("TEST\nTEST\fTEST", 0, "\u281e\u2811\u280e\u281e\n\u281e\u2811\u280e\u281e\f\u281e\u2811\u280e\u281e")
+    ("TEST\nTEST\fTEST", 0, "\u281e\u2811\u280e\u281e\n\u281e\u2811\u280e\u281e\f\u281e\u2811\u280e\u281e"),
+    ("\"S TEXT", 0, "\u2810\u280e\u2800\u281e\u2811\u282d\u281e"),
+    (r""" A1B'K2L@CIF/MSP"E3H9O6R^DJG>NTQ,*5<-U8V.%[$+X!&;:4\0Z7(_?W]#Y)=""", 0, "".join([chr(x) for x in range(0x2800, 0x2840)]))
 ])
 def test_convert_ascii_to_unicode_braille(text: str, cursor: int, expected_text: str):
     assert convert_ascii_to_unicode_braille(text, cursor, "Default") == DetectionResult(expected_text, len(text), "Default",
