80cb8c3992fe6d5e15bf3f43087debbf9ca244dc
Michael Whapples <mwhapples@aim.com>
2023-04-27 12:48:38 +0100

    Small refactoring

--- src/brf2ebrf/parser.py
+++ src/brf2ebrf/parser.py
@@ -23,8 +23,7 @@ class ParserPass:
 def parse(brf: str, parser_passes: Iterable[ParserPass]) -> str:
     text = brf
     for parser_pass in parser_passes:
-        cursor, state, selector = 0, parser_pass.initial_state, parser_pass.selector
-        text_builder = ""
+        text_builder, cursor, state, selector = "", 0, parser_pass.initial_state, parser_pass.selector
         while cursor < len(text):
             result = selector(text, cursor, state, parser_pass.detectors)
             text_builder, cursor, state = text_builder + result.text, result.cursor, result.state
