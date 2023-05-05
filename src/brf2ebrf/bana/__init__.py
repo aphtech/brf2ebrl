7986069239a48d124c0c634f0265340b85224e93
Michael Whapples <mwhapples@aim.com>
2023-05-05 16:51:44 +0100

    Make page number detection more flexible so it will be possible to apply to unicode Braille

--- src/brf2ebrf/bana/__init__.py
+++ src/brf2ebrf/bana/__init__.py
@@ -25,32 +25,34 @@ class PageNumberPosition(Enum):
 
 
 class BraillePageDetector:
-    def __init__(self, number_position: PageNumberPosition = PageNumberPosition.NONE):
+    def __init__(self, number_position: PageNumberPosition = PageNumberPosition.NONE, cells_per_line: int = 40, lines_per_page: int = 25, space_char: str = " "):
         super().__init__()
         self._number_position = number_position
+        self._cells_per_line = cells_per_line
+        self._lines_per_page = lines_per_page
+        self._space_char = space_char
 
-    def _find_page_number(self, page_content: str, cells_per_line: int, lines_per_page: int) -> tuple[str, str]:
+    def _find_page_number(self, page_content: str) -> tuple[str, str]:
         if self._number_position:
             lines = page_content.splitlines()
-            line_index = 0 if self._number_position.is_top() else (lines_per_page - 1)
+            line_index = 0 if self._number_position.is_top() else (self._lines_per_page - 1)
             if len(lines) > line_index:
                 line = lines[line_index]
                 left = self._number_position.is_left()
-                if left or len(line) >= cells_per_line:
-                    parted = line.partition("  ") if left else line.rpartition("  ")
-                    line, page_num = (parted[2].lstrip(), parted[0]) if left else (parted[0].rstrip(), parted[2])
-                    if page_num:
+                if left or len(line) >= self._cells_per_line:
+                    parted = line.partition(self._space_char * 3) if left else line.rpartition(self._space_char * 3)
+                    line, page_num = (parted[2].lstrip(self._space_char), parted[0]) if left else (parted[0].rstrip(self._space_char), parted[2])
+                    if parted[1] and page_num:
                         lines[line_index] = line
                         return "\n".join(lines), page_num
         return page_content, ""
 
-    def __call__(self, text: str, cursor: int, state: str, output_text: str, cells_per_line: int = 40,
-                 lines_per_page: int = 25) -> DetectionResult:
+    def __call__(self, text: str, cursor: int, state: str, output_text: str) -> DetectionResult:
         if state == "StartBraillePage":
             end_of_page = text.find("\f", cursor)
             page_content, new_cursor = (text[cursor:end_of_page], end_of_page + 1) if end_of_page >= 0 else (
                 text[cursor:], len(text))
-            page_content, page_num = self._find_page_number(page_content, cells_per_line, lines_per_page)
+            page_content, page_num = self._find_page_number(page_content)
             number_data = {"Number": page_num} if page_num else {}
             page_cmd = json.dumps({"BraillePage": number_data})
             output = f"\ue000{page_cmd}\ue001{page_content}"
