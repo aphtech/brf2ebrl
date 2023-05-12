6ea0ca0e74b5e1187fb15fc877380661655fbf4a
Michael Whapples <mwhapples@aim.com>
2023-05-12 15:17:40 +0100

    Convert Braille page numner detector to a function

--- src/brf2ebrf/bana/__init__.py
+++ src/brf2ebrf/bana/__init__.py
@@ -4,8 +4,8 @@ import string
 
 from brf2ebrf.parser import DetectionResult
 
-
 _BRL_WHITESPACE = string.whitespace + "\u2800"
+
 
 class PageNumberPosition(Enum):
     NONE = 0
@@ -27,41 +27,37 @@ class PageNumberPosition(Enum):
         return self.value.bit_count() == 2
 
 
-def create_braille_page_detector(number_position: PageNumberPosition = PageNumberPosition.NONE, cells_per_line: int = 40, lines_per_page: int = 25, separator: str = "   "):
-    return BraillePageDetector(number_position, cells_per_line, lines_per_page, separator)
+def _find_page_number(page_content: str, number_position: PageNumberPosition, cells_per_line: int, lines_per_page: int,
+                      separator: str) -> tuple[str, str]:
+    if number_position:
+        lines = page_content.splitlines()
+        line_index = 0 if number_position.is_top() else (lines_per_page - 1)
+        if len(lines) > line_index:
+            line = lines[line_index]
+            left = number_position.is_left()
+            if left or len(line) >= cells_per_line:
+                parted = line.partition(separator) if left else line.rpartition(separator)
+                line, page_num = (parted[2].lstrip(_BRL_WHITESPACE), parted[0]) if left else (
+                    parted[0].rstrip(_BRL_WHITESPACE), parted[2])
+                if parted[1] and page_num:
+                    lines[line_index] = line
+                    return "\n".join(lines), page_num
+    return page_content, ""
 
 
-class BraillePageDetector:
-    def __init__(self, number_position: PageNumberPosition = PageNumberPosition.NONE, cells_per_line: int = 40, lines_per_page: int = 25, separator: str = "   "):
-        super().__init__()
-        self._number_position = number_position
-        self._cells_per_line = cells_per_line
-        self._lines_per_page = lines_per_page
-        self._separator = separator
-
-    def _find_page_number(self, page_content: str) -> tuple[str, str]:
-        if self._number_position:
-            lines = page_content.splitlines()
-            line_index = 0 if self._number_position.is_top() else (self._lines_per_page - 1)
-            if len(lines) > line_index:
-                line = lines[line_index]
-                left = self._number_position.is_left()
-                if left or len(line) >= self._cells_per_line:
-                    parted = line.partition(self._separator) if left else line.rpartition(self._separator)
-                    line, page_num = (parted[2].lstrip(_BRL_WHITESPACE), parted[0]) if left else (parted[0].rstrip(_BRL_WHITESPACE), parted[2])
-                    if parted[1] and page_num:
-                        lines[line_index] = line
-                        return "\n".join(lines), page_num
-        return page_content, ""
-
-    def __call__(self, text: str, cursor: int, state: str, output_text: str) -> DetectionResult:
+def create_braille_page_detector(number_position: PageNumberPosition = PageNumberPosition.NONE,
+                                 cells_per_line: int = 40, lines_per_page: int = 25, separator: str = "   "):
+    def detect_braille_page_number(text: str, cursor: int, state: str, output_text: str) -> DetectionResult:
         if state == "StartBraillePage":
             end_of_page = text.find("\f", cursor)
             page_content, new_cursor = (text[cursor:end_of_page], end_of_page + 1) if end_of_page >= 0 else (
                 text[cursor:], len(text))
-            page_content, page_num = self._find_page_number(page_content)
+            page_content, page_num = _find_page_number(page_content, number_position, cells_per_line,
+                                                       lines_per_page, separator)
             number_data = {"Number": page_num} if page_num else {}
             page_cmd = json.dumps({"BraillePage": number_data})
             output = f"\ue000{page_cmd}\ue001{page_content}"
             return DetectionResult(cursor=new_cursor, state=state, confidence=1.0, text=output_text + output)
         return DetectionResult(cursor + 1, state, 0.0, output_text + text[cursor])
+
+    return detect_braille_page_number
