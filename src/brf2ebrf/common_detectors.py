6c89095fa02a8fbbf7693bbec65a392641d90ae1
Michael Whapples <mwhapples@aim.com>
2023-05-17 18:05:24 +0100

    Add a detector for passing through processing instructions

--- src/brf2ebrf/common_detectors.py
+++ src/brf2ebrf/common_detectors.py
@@ -15,3 +15,12 @@ def convert_ascii_to_unicode_braille_bulk(text: str, cursor: int, state: str, ou
 def convert_ascii_to_unicode_braille(text: str, cursor: int, state: str, output_text: str) -> DetectionResult:
     """Convert oly th next character to unicode Braille."""
     return DetectionResult(cursor + 1, state, 1.0, output_text + text[cursor].translate(_ASCII_TO_UNICODE_DICT))
+
+
+def detect_and_pass_processing_instructions(text: str, cursor: int, state: str, output_text: str) -> DetectionResult:
+    """Detect and pass through processing instructions"""
+    if text.startswith("<?", cursor):
+        end_of_pi = text.find("?>", cursor) + 2
+        if end_of_pi >= 4:
+            return DetectionResult(end_of_pi, state, confidence=0.9, text=output_text + text[cursor:end_of_pi])
+    return DetectionResult(cursor + 1, state, confidence=0.0, text=output_text + text[cursor])
