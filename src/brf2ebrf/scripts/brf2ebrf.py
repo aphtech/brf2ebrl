5308878b08f6c4c0bec5ad68384cc212d113ce83
Ken Perry <kperry@aph.org>
2023-06-05 07:28:19 -0400

    Added xhtml default header and footer

--- src/brf2ebrf/scripts/brf2ebrf.py
+++ src/brf2ebrf/scripts/brf2ebrf.py
@@ -6,7 +6,18 @@ from brf2ebrf.common.detectors import convert_ascii_to_unicode_braille_bulk, det
 from brf2ebrf.common.selectors import most_confident_detector
 from brf2ebrf.parser import parse, ParserPass
 
+xhtml_header = """
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/strict.dtd">
+<html xmlns="http://www.w3.org/TR/xhtml1/strict" >
+<body>
+"""
 
+xhtml_footer= """
+</body>
+</html>
+"""
 def main():
     arg_parser = argparse.ArgumentParser(description="Converts a BRF to eBRF")
     arg_parser.add_argument("brf", help="The BRF to convert")
@@ -25,7 +36,7 @@ def main():
                               ParserPass("Default", [convert_unknown_to_pre], most_confident_detector)
                               ])
     with open(args.output_file, "w", encoding="utf-8") as out_file:
-        out_file.write(output_text)
+        out_file.write(f"{xhtml_header}{output_text}{xhtml_footer}")
 
 
 if __name__ == "__main__":
