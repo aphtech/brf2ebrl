4c9287863a764549b9ed96dfaf3c085d32c00f22
Michael Whapples <mwhapples@aim.com>
2023-06-28 14:44:40 +0100

    An intermediate commit working towards limiting running head so it does not detect for first pages of a given type

--- src/brf2ebrf/common/detectors.py
+++ src/brf2ebrf/common/detectors.py
@@ -1,5 +1,6 @@
 """Some detectors common to multiple Braille codes/standards."""
 import re
+from enum import Enum, auto
 from typing import Optional
 
 from brf2ebrf.parser import DetectionResult, DetectionState, Detector
@@ -8,6 +9,13 @@ _ASCII_TO_UNICODE_DICT = str.maketrans(
     r""" A1B'K2L@CIF/MSP"E3H9O6R^DJG>NTQ,*5<-U8V.%[$+X!&;:4\0Z7(_?W]#Y)=""",
     "".join([chr(x) for x in range(0x2800, 0x2840)])
 )
+
+
+class BraillePageType(Enum):
+    UNSET = 0
+    T = auto()
+    P = auto()
+    NORMAL = auto()
 
 
 def convert_ascii_to_unicode_braille_bulk(text: str, cursor: int, state: DetectionState, output_text: str) -> DetectionResult:
@@ -37,10 +45,13 @@ def convert_blank_line_to_pi(text: str, cursor: int, state: DetectionState, outp
 
 def create_running_head_detector(min_indent: int) -> Detector:
     """Create a detector for running heads."""
-    min_indent_re = re.compile(f"(<\\?braille-page [\u2800-\u28ff]*\\?>)\u2800{{{min_indent},}}([\u2801-\u28ff][\u2800-\u28ff]*)[\n\f]+")
+    min_indent_re = re.compile(f"(?P<braille_page_pi><\\?braille-page (?P<brl_page_num>[\u2800-\u28ff]*)\\?>)\u2800{{{min_indent},}}(?P<running_head>[\u2801-\u28ff][\u2800-\u28ff]*)[\n\f]")
     def detect_running_head(text: str, cursor: int, state: DetectionState, output_text: str) -> Optional[
         DetectionResult]:
         if m := min_indent_re.match(text[cursor:]):
-            return DetectionResult(cursor + m.end(), state, 1.0, f"{output_text}{m.group(1)}<?running-head {m.group(2)}?>")
+            braille_page_pi, brl_page_num, running_head = (m.group(x) for x in ["braille_page_pi", "brl_page_num", "running_head"])
+            prev_braille_page_type = state.get("braille-page-type", default=BraillePageType.UNSET)
+            braille_page_type = BraillePageType.T if brl_page_num.startswith("\u281e") else BraillePageType.P if brl_page_num.startswith("\u280f") else BraillePageType.NORMAL if brl_page_num else prev_braille_page_type
+            return DetectionResult(cursor + m.end(), state, 1.0, f"{output_text}{braille_page_pi}<?running-head {running_head}?>")
         return None
     return detect_running_head
