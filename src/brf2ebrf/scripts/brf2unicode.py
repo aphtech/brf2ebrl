b865ca6dec245211c10a91358f8804a145a13487
Ken Perry <kperry@aph.org>
2023-07-05 06:55:05 -0400

    change from readline to read in brf2unicode.

--- src/brf2ebrf/scripts/brf2unicode.py
+++ src/brf2ebrf/scripts/brf2unicode.py
@@ -7,15 +7,23 @@ from brf2ebrf.parser import parse, ParserPass
 
 
 def main():
-    arg_parser = argparse.ArgumentParser(description="Converts a BRF to unicode Braille")
+    arg_parser = argparse.ArgumentParser(
+        description="Converts a BRF to unicode Braille"
+    )
     arg_parser.add_argument("brf", help="The BRF to convert")
     arg_parser.add_argument("output_file", help="The output file name")
     args = arg_parser.parse_args()
     brf = ""
     with open(args.brf, "r", encoding="utf-8") as in_file:
-        for line in in_file.readlines():
-            brf += line
-    output_text = parse(brf, [ParserPass({}, [convert_ascii_to_unicode_braille_bulk], most_confident_detector)])
+        brf = in_file.read()
+    output_text = parse(
+        brf,
+        [
+            ParserPass(
+                {}, [convert_ascii_to_unicode_braille_bulk], most_confident_detector
+            )
+        ],
+    )
     with open(args.output_file, "w", encoding="utf-8") as out_file:
         out_file.write(output_text)
 
