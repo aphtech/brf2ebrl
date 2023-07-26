ec844b930459dc4ccc4637f9c7f981828983e64c
Michael Whapples <mwhapples@aim.com>
2023-07-26 16:16:21 +0100

    Detect print page numbers correctly and insert them into processing instructions

--- src/brf2ebrf/bana/__init__.py
+++ src/brf2ebrf/bana/__init__.py
@@ -11,11 +11,11 @@ _BRL_WHITESPACE = string.whitespace + "\u2800"
 
 
 def _find_page_number(
-    page_content: str,
-    number_position: PageNumberPosition,
-    cells_per_line: int,
-    lines_per_page: int,
-    separator: str,
+        page_content: str,
+        number_position: PageNumberPosition,
+        cells_per_line: int,
+        lines_per_page: int,
+        separator: str,
         number_filter: Callable[[str], bool] = lambda n: True,
 ) -> tuple[str, str]:
     if number_position:
@@ -47,15 +47,16 @@ def _create_braille_page_command(page_content: str, page_num: str) -> str:
 
 
 def create_braille_page_detector(
-    page_layout: PageLayout,
-    separator: str = "   ",
-    format_output: Callable[[str, str], str] = _create_braille_page_command,
+        page_layout: PageLayout,
+        separator: str = "   ",
+        format_output: Callable[[str, str], str] = _create_braille_page_command,
 ) -> Detector:
     """Factory function to create a detector for Braille page numbers."""
-    braille_page_number_pattern = re.compile("[\u280f\u281e]?\u283c[\u2801\u2803\u2809\u2819\u2811\u280b\u281b\u2813\u280a\u281a]+")
+    braille_page_number_pattern = re.compile(
+        "[\u280f\u281e]?\u283c[\u2801\u2803\u2809\u2819\u2811\u280b\u281b\u2813\u280a\u281a]+")
 
     def detect_braille_page_number(
-        text: str, cursor: int, state: DetectionState, output_text: str
+            text: str, cursor: int, state: DetectionState, output_text: str
     ) -> DetectionResult | None:
         if state.get("StartBraillePage", False):
             page_content = text[cursor:].split("\f")[0]
@@ -84,25 +85,61 @@ def create_braille_page_detector(
     return detect_braille_page_number
 
 
-def create_print_page_detector(page_layout: PageLayout, separator: str = "\u2800"*3) -> Detector:
+_CONTINUATION_LETTERS = "\u2801\u2803\u2809\u2819\u2811\u280b\u281b\u2813\u280a\u281a\u2805\u2807\u280d\u281d\u2815\u280f\u281f\u2817\u280e\u281e\u2825\u2827\u283a\u282d\u283d\u2835"
+
+
+def _convert_int_to_continuation_letter(x: int) -> str:
+    """Creates the continuation letter string representing the integer value."""
+    result = ""
+    while letter := x % len(_CONTINUATION_LETTERS):
+        result = _CONTINUATION_LETTERS[letter - 1] + result
+        x = x // len(_CONTINUATION_LETTERS)
+    return result
+
+
+def _is_continuation_number(page_num: str, prev_page_num: str, continuation: int) -> bool:
+    return prev_page_num and page_num == _convert_int_to_continuation_letter(continuation) + prev_page_num
+
+
+_PRINT_PAGE_NUMBER_LINE_RE = re.compile("\u2824{5,}(?P<ppn>[\u2800-\u28ff]+)")
+
+
+def _is_print_page_number_line(line: str) -> str | None:
+    match = _PRINT_PAGE_NUMBER_LINE_RE.fullmatch(line)
+    return match.group("ppn") if match else None
+
+
+def create_print_page_detector(page_layout: PageLayout, separator: str = "\u2800" * 3) -> Detector:
     """Create a detector for print page numbers."""
-    def is_continuation_number(page_num: str, prev_page_num: str, continuation_letter: str) -> bool:
-        # Todo implementation
-        return False
 
-    print_page_number_line_re = re.compile("\u2824{5,}(?P<ppn>[\u2800-\u28ff]+)")
-    def is_print_page_number_line(line: str) -> str | None:
-        match = print_page_number_line_re.fullmatch(line)
-        return match.group("ppn") if match else None
-
-    def detect_print_page_number(text: str, cursor: int, state: DetectionState, output_text: str) -> DetectionResult | None:
+    def detect_print_page_number(text: str, cursor: int, state: DetectionState,
+                                 output_text: str) -> DetectionResult | None:
         if ord(text[cursor]) in range(0x2800, 0x2900):
             page_content = text[cursor:].partition("\f")[0]
             new_cursor = cursor + len(page_content)
-            page_content, page_num = _find_page_number(page_content, page_layout.print_page_number, page_layout.cells_per_line, page_layout.lines_per_page, separator)
-            result = "" if is_continuation_number(page_num, "", "") else f"<?print_page {page_num}?>"
-            result += page_content
-            result += "\n".join([f"<?print-page {ppn}?>" if len(line) == page_layout.cells_per_line and (ppn := is_print_page_number_line(line)) else line for line in page_content.splitlines()])
-            return DetectionResult(new_cursor, state, 0.9, f"{output_text}{result}")
+            page_content, page_num = _find_page_number(page_content, page_layout.print_page_number,
+                                                       page_layout.cells_per_line, page_layout.lines_per_page,
+                                                       separator)
+            s_ppn = state.get("ppn", "")
+            s_cont = state.get("continuation", 0)
+            result = ""
+            if page_num:
+                s_cont += 1
+                if not _is_continuation_number(page_num, s_ppn, s_cont):
+                    s_cont = 0
+                    s_ppn = page_num
+                    result += f"<?print-page {page_num}?>"
+            lines = []
+            for line in page_content.splitlines():
+                if len(line) == page_layout.cells_per_line and (ppn := _is_print_page_number_line(line)):
+                    s_ppn = ppn
+                    s_cont = 0
+                    lines.append(f"<?print-page {ppn}?>")
+                else:
+                    lines.append(line)
+            result += "\n".join(lines)
+            return DetectionResult(new_cursor, dict(state, ppn=s_ppn, continuation=s_cont), 0.9,
+                                   f"{output_text}{result}")
         return None
+
     return detect_print_page_number
