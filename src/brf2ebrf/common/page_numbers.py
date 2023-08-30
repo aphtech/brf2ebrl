0191988b18c164f29d9dfdcfc43ee31ed12623d3
Michael Whapples <mwhapples@aim.com>
2023-08-30 13:24:11 +0100

    Handle div, table and ul after a print page number.

--- src/brf2ebrf/common/page_numbers.py
+++ src/brf2ebrf/common/page_numbers.py
@@ -3,6 +3,7 @@ import re
 from brf2ebrf.parser import DetectionState, DetectionResult, Detector
 
 _PRINT_PAGE_RE = re.compile("<\\?print-page (?P<page_number>[\u2800-\u28ff]*)\\?>")
+_FIND_FOLLOWING_BLOCK_RE = re.compile("((<\\?blank-line\\?>)|\n)*<((h[1-6])|p|(pre)|(table)|(ul)|(div))(\\s|>)")
 _NON_NESTED_BLOCKS_RE = re.compile("((<\\?blank-line\\?>)|\n)*<(?P<tag_name>(h[1-6])|p|(pre))(.|\n)*?</(?P=tag_name)>")
 _TAG_NAME_PATTERN = "[_a-zA-Z][-_.a-zA-Z0-9]*"
 _ELEMENT_TAG_RE = re.compile(f"(<(?P<start_tag_name>{_TAG_NAME_PATTERN})\\s*(/>)?)|(</(?P<end_tag_name>{_TAG_NAME_PATTERN})>)")
@@ -35,9 +36,12 @@ def create_ebrf_print_page_tags() -> Detector:
             if m.start() > cursor:
                 return DetectionResult(m.start(), state, 0.9, f"{output_text}{text[cursor:m.start()]}")
             page_number = m.group("page_number")
-            if tag_match := _NON_NESTED_BLOCKS_RE.match(text, m.end()):
-                return DetectionResult(tag_match.end(), state, 0.9, f"{output_text}<div class=\"keeptgr\"><span role=\"doc-pagebreak\" id=\"page{page_number}\" class=\"keepwithnext\">{page_number}</span>{tag_match.group()}</div>")
-            return DetectionResult(m.end(), state, 0.9,
+            tag_start = m.end()
+            if _FIND_FOLLOWING_BLOCK_RE.match(text, tag_start):
+                end_index = _find_end_of_element(text, tag_start)
+                if end_index > tag_start:
+                    return DetectionResult(end_index, state, 0.9, f"{output_text}<div class=\"keeptgr\"><span role=\"doc-pagebreak\" id=\"page{page_number}\" class=\"keepwithnext\">{page_number}</span>{text[tag_start:end_index]}</div>")
+            return DetectionResult(tag_start, state, 0.9,
                                    f"{output_text}<span role=\"doc-pagebreak\" id=\"page{page_number}\">{page_number}</span>")
         return DetectionResult(len(text), state, 0.5, f"{output_text}{text[cursor:]}")
     return convert_to_ebrf_print_page_numbers
