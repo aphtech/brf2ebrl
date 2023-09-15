6c8c6686a198888190e30ba9835de542cebd9e11
Ken Perry <kperry@aph.org>
2023-09-15 09:35:17 -0400

    put in the regular expressions

--- src/brf2ebrf/common/graphic_detectors.py
+++ src/brf2ebrf/common/graphic_detectors.py
@@ -1,5 +1,5 @@
 """Detector for Graphics"" currently for PDF"""
-import re
+import regex as re
 from collections.abc import Iterable
 import logging
 import sys
@@ -11,6 +11,8 @@ from brf2ebrf.parser import DetectionState, DetectionResult, Detector
 
 def create_images_references(filename_path: str, images_path: str) -> dict:
     """Creates the PDF files and the references dictionary. Returns the Reference dictionary"""
+    _braille_page_re = re.compile(r"([a-zA-Z]*#+[a-zA-z]*)$")
+    _references = {}
     in_folder, in_filename_base = os.path.split(filename_path)
     if not in_folder or not os.path.isdir(in_folder):
         output_folder = os.getcwd()
@@ -32,7 +34,7 @@ def create_images_references(filename_path: str, images_path: str) -> dict:
         _files = [
             os.path.join(images_path, s)
             for s in os.listdir(images_path)
-            if re.match(f"{output_filename}.*\.pdf", s, re.IGNORECASE)
+            if re.match(f"{in_filename_base}.*\.pdf", s, re.IGNORECASE)
         ]
         if _files:
             images_path = _files[0]
@@ -41,56 +43,74 @@ def create_images_references(filename_path: str, images_path: str) -> dict:
     if not images_path or (images_path and not os.path.exists(images_path)):
         logging.error(f"No images path or folder found {images_path}")
         sys.exit()
-
-    #visitor code for the pdf parser
+    # visitor code for the pdf parser
     _parts = []
-def visitor_body(text, cm, tm, fontDict, fontSize):
-    y = tm[5]
-    if y <50 and text.strip(' \n\r\l\f'):
-        text = text.strip('_:')
-        parts.append(text.strip(' \n\r\l\f'))
-    
-    def write_pdf(base_filename,bp_page_number,pdf_page,left = False):
-    fuck
+
+    def visitor_body(text, cm, tm, fontDict, fontSize):
+        y = tm[5]
+        if y < 50 and text.strip(" \n\r\l\f"):
+            text = text.strip("_:")
+            parts.append(text.strip(" \n\r\l\f"))
+
+    def write_pdf(bp_page_number, pdf_filename, pdf_page):
+        logging.info(f"page: {bp_page_number} pdf_file ref {pdf_filename}")
+        if bp_page_number in _references.keys():
+            _references[bp_page_number].append(pdf_filename)
+        else:
+            _references[bp_page_number] = [pdf_filename]
+
         output = PdfWriter()
-        output.add_page(pdf_page
-        with open (f"{base_filename}.pdf","wb") as outputStream:
+        output.add_page(pdf_page)
+        with open(pdf_filename, "wb") as outputStream:
             output.write(outputStream)
-    
-    
-    inputpdf = PdfReader(open(filename_path,"rb"))
+
+    logging.info("log 3")
+    inputpdf = PdfReader(open(images_path, "rb"))
     left_page = False
-    while page_number in range(len(inputpdf.pages)):
-        parts=[]
+    for page_number in range(len(inputpdf.pages)):
+        parts = []
         inputpdf.pages[page_number].extract_text(visitor_text=visitor_body)
-        braille_page_number  = "\n".join(parts)
-        parts = braille_page_number.strip(' \n\r\l\f%').split()
+        braille_page_number = "\n".join(parts)
+        parts = braille_page_number.strip(" \n\r\l\f%").split()
         if parts and _braille_page_re.match(parts[-1]):
-            braille_page_number =  parts[-1].strip()
+            braille_page_number = parts[-1].strip()
             if left_page:
-                write_pdf(braille_page_number,f"{output_folder}_{in_filename_base}_l",inputpdf.pages[page_number-1])
-            write_pdf(braille_page_number,f"{output_folder}_{in_filename_base}",inputpdf.pages[page_number])
+                write_pdf(
+                    braille_page_number,
+                    f"{output_folder}/{in_filename_base}_{braille_page_number.strip('#')}_l.pdf",
+                    inputpdf.pages[page_number - 1],
+                )
+            write_pdf(
+                braille_page_number,
+                f"{output_folder}/{in_filename_base}_{braille_page_number.strip('#')}.pdf",
+                inputpdf.pages[page_number],
+            )
             left_page = False
         else:
-            left_page = True 
+            left_page = True
     return _references
 
 
-def create_pdf_graphic_detector(file_paths: list) -> Detector:
+def create_pdf_graphic_detector(brf_filename: str, images_path: str) -> Detector:
     """Creates a detector for finding graphic page numbers and matching with PDF pages
     * argument:
     * filename None if no image file or folder
     *Returns:
     * DetectionResult,
     """
-    _images_references = create_images_references(file_paths[0], file_paths[1])
+
+    # image references
+    _images_references = create_images_references(brf_filename, images_path)
+
+    # regular expression matching blanks then a braile page number.
+    _blank_lines = "(?:(\n<\\?blank-line\\?>))+"
+    _braille_page_re = re.compile(f"{_blank_lines}(<\\?braille-page.*\\?>)")
 
     def detect_pdf(
         text: str, cursor: int, state: DetectionState, output_text: str
     ) -> DetectionResult | None:
-        if cursor % 10000 == 0:
-            _images_references["a"] = "one"
+        if cursor % 80000 == 0:
             print(f"Cursor: {cursor} {_images_references}")
-            return None
+        return None
 
     return detect_pdf
--- src/brf2ebrf/scripts/brf2ebrf.py
+++ src/brf2ebrf/scripts/brf2ebrf.py
@@ -1,10 +1,12 @@
 """Script to convert BRF into eBRF."""
 import argparse
+import os
 import logging
 from collections.abc import Iterable
 
 from brf2ebrf.bana import create_braille_page_detector, create_print_page_detector
 from brf2ebrf.common import PageNumberPosition, PageLayout
+from brf2ebrf.common.graphic_detectors import create_pdf_graphic_detector
 from brf2ebrf.common.block_detectors import (
     detect_pre,
     create_cell_heading,
@@ -17,7 +19,8 @@ from brf2ebrf.common.detectors import (
     convert_ascii_to_unicode_braille_bulk,
     detect_and_pass_processing_instructions,
     convert_blank_line_to_pi,
-    create_running_head_detector, braille_page_counter_detector,
+    create_running_head_detector,
+    braille_page_counter_detector,
 )
 from brf2ebrf.common.page_numbers import create_ebrf_print_page_tags
 from brf2ebrf.common.selectors import most_confident_detector
@@ -36,98 +39,166 @@ _XHTML_FOOTER = """</body>
 
 
 def create_brf2ebrf_parser(
-        page_layout: PageLayout = PageLayout(),
-        detect_running_heads: bool = True,
+    page_layout: PageLayout = PageLayout(),
+    brf_filename: str = "",
+    images_path: str = "",
+    detect_running_heads: bool = True,
 ) -> Iterable[ParserPass]:
-    return [x for x in [
-        # Convert to unicode pass
-        ParserPass(
-            "Convert to unicode Braille", {}, [convert_ascii_to_unicode_braille_bulk], most_confident_detector
-        ),
-        # Detect Braille pages pass
-        ParserPass(
-            "Detect Braille pages", {"StartBraillePage": True},
-            [
-                create_braille_page_detector(
-                    page_layout=page_layout,
-                    separator="\u2800" * 3,
-                    format_output=lambda pc, pn: f"<?braille-page {pn}?>{pc}",
-                ),
-                detect_and_pass_processing_instructions,
-            ],
-            most_confident_detector,
-        ),
-        ParserPass(
-            "Detect print pages", {},
-            [
-                create_print_page_detector(
-                    page_layout=page_layout, separator="\u2800" * 3
-                ),
-                detect_and_pass_processing_instructions,
-            ],
-            most_confident_detector,
-        ),
-        # Running head pass
-        ParserPass(
-            "Detect running head", {},
-            [create_running_head_detector(3), braille_page_counter_detector, detect_and_pass_processing_instructions],
-            most_confident_detector
-        ) if detect_running_heads else None,
-        # Remove form feeds pass.
-        ParserPass(
-            "Remove form feeds", {},
-            [
-                lambda t, c, s, o: DetectionResult(
-                    len(t), s, 1.0, o + t[c:].replace("\f", "\n")
-                )
-            ],
-            most_confident_detector,
-        ),
-        # Detect blank lines pass
-        ParserPass(
-            "Detect blank lines", {},
-            [convert_blank_line_to_pi, detect_and_pass_processing_instructions],
-            most_confident_detector,
-        ),
-        # Detect blocks pass
-        ParserPass(
-            "Detect blocks", {},
-            [
-                create_centered_detector(page_layout.cells_per_line, 3, "h1"),
-                create_cell_heading(6, "h3"),
-                create_cell_heading(4, "h2"),
-                create_paragraph_detector(2, 0),
-                create_table_detector(),  # might add arguments later
-                create_list_detector(0, 2),
-                detect_pre,
-                detect_and_pass_processing_instructions,
-            ],
-            most_confident_detector,
-        ),
-        # Convert print page numbers to ebrf tags
-        ParserPass("Print page numbers to ebrf", {}, [create_ebrf_print_page_tags()], most_confident_detector),
-        # Make complete XHTML pass
-        ParserPass(
-            "Make complete XML", {},
-            [
-                lambda t, c, s, o: DetectionResult(
-                    len(t), s, 1.0, f"{o}{_XHTML_HEADER}{t[c:]}{_XHTML_FOOTER}"
-                )
-            ],
-            most_confident_detector,
-        ),
-    ] if x is not None]
+    return [
+        x
+        for x in [
+            # Convert to unicode pass
+            ParserPass(
+                "Convert to unicode Braille",
+                {},
+                [convert_ascii_to_unicode_braille_bulk],
+                most_confident_detector,
+            ),
+            # Detect Braille pages pass
+            ParserPass(
+                "Detect Braille pages",
+                {"StartBraillePage": True},
+                [
+                    create_braille_page_detector(
+                        page_layout=page_layout,
+                        separator="\u2800" * 3,
+                        format_output=lambda pc, pn: f"<?braille-page {pn}?>{pc}",
+                    ),
+                    detect_and_pass_processing_instructions,
+                ],
+                most_confident_detector,
+            ),
+            ParserPass(
+                "Detect print pages",
+                {},
+                [
+                    create_print_page_detector(
+                        page_layout=page_layout, separator="\u2800" * 3
+                    ),
+                    detect_and_pass_processing_instructions,
+                ],
+                most_confident_detector,
+            ),
+            # Running head pass
+            ParserPass(
+                "Detect running head",
+                {},
+                [
+                    create_running_head_detector(3),
+                    braille_page_counter_detector,
+                    detect_and_pass_processing_instructions,
+                ],
+                most_confident_detector,
+            )
+            if detect_running_heads
+            else None,
+            # Remove form feeds pass.
+            ParserPass(
+                "Remove form feeds",
+                {},
+                [
+                    lambda t, c, s, o: DetectionResult(
+                        len(t), s, 1.0, o + t[c:].replace("\f", "\n")
+                    )
+                ],
+                most_confident_detector,
+            ),
+            # Detect blank lines pass
+            ParserPass(
+                "Detect blank lines",
+                {},
+                [convert_blank_line_to_pi, detect_and_pass_processing_instructions],
+                most_confident_detector,
+            ),
+            # Detect blocks pass
+            ParserPass(
+                "Detect blocks",
+                {},
+                [
+                    create_centered_detector(page_layout.cells_per_line, 3, "h1"),
+                    create_cell_heading(6, "h3"),
+                    create_cell_heading(4, "h2"),
+                    create_paragraph_detector(2, 0),
+                    create_table_detector(),  # might add arguments later
+                    create_list_detector(0, 2),
+                    detect_pre,
+                    detect_and_pass_processing_instructions,
+                ],
+                most_confident_detector,
+            ),
+            # PDF Graphics
+            ParserPass(
+                "Print PDF to Links and single page PDF",
+                {},
+                [create_pdf_graphic_detector(brf_filename, images_path)],
+                most_confident_detector,
+            ) if images_path else None,
+            # Convert print page numbers to ebrf tags
+            ParserPass(
+                "Print page numbers to ebrf",
+                {},
+                [create_ebrf_print_page_tags()],
+                most_confident_detector,
+            ),
+            # Make complete XHTML pass
+            ParserPass(
+                "Make complete XML",
+                {},
+                [
+                    lambda t, c, s, o: DetectionResult(
+                        len(t), s, 1.0, f"{o}{_XHTML_HEADER}{t[c:]}{_XHTML_FOOTER}"
+                    )
+                ],
+                most_confident_detector,
+            ),
+        ]
+        if x is not None
+    ]
 
 
 def main():
-    logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(asctime)s:%(module)s:%(message)s")
+    logging.basicConfig(
+        level=logging.INFO, format="%(levelname)s:%(asctime)s:%(module)s:%(message)s"
+    )
     arg_parser = argparse.ArgumentParser(description="Converts a BRF to eBRF")
-    arg_parser.add_argument("--no-running-heads", help="Don't detect running heads", dest="running_heads", default=True, action="store_false")
-    arg_parser.add_argument("-cpl, --cells-per-line", help="Number of cells per line", dest="cells_per_line", default=40, type=int)
-    arg_parser.add_argument("-lpp, --lines-per-page", help="Number of lines per page", dest="lines_per_page", default=25, type=int)
+    arg_parser.add_argument(
+        "--no-running-heads",
+        help="Don't detect running heads",
+        dest="running_heads",
+        default=True,
+        action="store_false",
+    )
+    arg_parser.add_argument(
+        "-cpl, --cells-per-line",
+        help="Number of cells per line",
+        dest="cells_per_line",
+        default=40,
+        type=int,
+    )
+    arg_parser.add_argument(
+        "-lpp, --lines-per-page",
+        help="Number of lines per page",
+        dest="lines_per_page",
+        default=25,
+        type=int,
+    )
+    arg_parser.add_argument(
+        "-i", "--images", type=str, help="The images folder or file."
+    )
     arg_parser.add_argument("brf", help="The BRF to convert")
     arg_parser.add_argument("output_file", help="The output file name")
     args = arg_parser.parse_args()
+
+    if not args.brf:
+        logging.error("No input Brf to be converted.")
+        arg_parser.print_help()
+        sys.exit()
+
+    if args.images and not os.path.exists(args.images):
+        logging.error(f"{args.images} is not a filename or folder.")
+        arg_parser.print_help()
+        sys.exit()
+
     page_layout = PageLayout(
         braille_page_number=PageNumberPosition.BOTTOM_RIGHT,
         print_page_number=PageNumberPosition.TOP_RIGHT,
@@ -137,7 +208,15 @@ def main():
     brf = ""
     with open(args.brf, "r", encoding="utf-8") as in_file:
         brf = in_file.read()
-    output_text = parse(brf, create_brf2ebrf_parser(page_layout=page_layout, detect_running_heads=args.running_heads))
+    output_text = parse(
+        brf,
+        create_brf2ebrf_parser(
+            page_layout=page_layout,
+            detect_running_heads=args.running_heads,
+            brf_filename=args.brf,
+            images_path=args.images,
+        ),
+    )
     with open(args.output_file, "w", encoding="utf-8") as out_file:
         out_file.write(output_text)
 
