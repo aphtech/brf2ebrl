b1e707e77f8eef9791841024681c11f5d92eaf2e
Michael Whapples <software@mdw.onl>
2024-01-24 10:33:44 +0000

    Detect inline TN

--- src/brf2ebrf/bana/tn_detectors.py
+++ src/brf2ebrf/bana/tn_detectors.py
@@ -9,11 +9,29 @@ import re
 from brf2ebrf.parser import DetectionState, DetectionResult
 from brf2ebrf.utils import find_end_of_element
 
+_START_TN_BLOCK = "<div class=\"tn\">"
+_END_TN_BLOCK = "</div>"
+_START_TN_SPAN = "<span class=\"tn\">"
+_END_TN_SPAN = "</span>"
+_START_TN_SYMBOL = "\u2808\u2828\u2823"
+_END_TN_SYMBOL = "\u2808\u2828\u281c"
+
 
 def tn_indicators_block_matcher(brl: str, state: DetectionState) -> (str | None, DetectionState):
-    if brl.startswith("\u2808\u2828\u2823") and brl.endswith("\u2808\u2828\u281c"):
-        return f"<div class=\"tn\">{brl}</div>", state
+    if brl.startswith(_START_TN_SYMBOL) and brl.endswith(_END_TN_SYMBOL):
+        return f"{_START_TN_BLOCK}{brl}{_END_TN_BLOCK}>", state
     return None, state
+
+
+_INLINE_TN_RE = re.compile(f"((?<!{_START_TN_BLOCK}){_START_TN_SYMBOL}[\u2800-\u28ff\\s]+{_END_TN_SYMBOL})")
+
+
+def detect_inline_tn(text: str, cursor: int, state: DetectionState, output_text: str) -> DetectionResult:
+    """Detect inline TNs.
+
+    This detector will process the entire text until the end."""
+    new_text = _INLINE_TN_RE.subn(f"{_START_TN_SPAN}\\1{_END_TN_SPAN}", text[cursor:])[0]
+    return DetectionResult(len(text), state, 1.0, f"{output_text}{new_text}")
 
 
 _TN_HEADING_START_RE = re.compile("<h3>\u2808\u2828\u2823")
@@ -32,8 +50,10 @@ def detect_symbols_list_tn(text: str, cursor: int, state: DetectionState, output
                     list_start = m.end()
                     if _TN_LIST_START_RE.match(text, list_start):
                         list_end = find_end_of_element(text, list_start)
-                        if list_end >= 0 and "".join(c for c in text[cursor:list_end] if "\u2800" < c <= "\u28ff").endswith("\u2808\u2828\u281c"):
+                        if list_end >= 0 and "".join(
+                                c for c in text[cursor:list_end] if "\u2800" < c <= "\u28ff").endswith(
+                                "\u2808\u2828\u281c"):
                             return DetectionResult(list_end, state, 0.95,
-                                                   f"{output_text}{text[cursor:position]}<div class=\"tn\">{text[position:list_end]}</div>")
+                                                   f"{output_text}{text[cursor:position]}{_START_TN_BLOCK}{text[position:list_end]}{_END_TN_BLOCK}")
         end += 1
     return DetectionResult(end, state, 0.5, f"{output_text}{text[cursor:end]}")
