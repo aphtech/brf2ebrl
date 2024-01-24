e2d6158facb1c4ac4a6ec453c8aa6199d78de4f2
Ken Perry <kperry@aph.org>
2024-01-24 10:32:22 -0500

    regular expression cleanup

--- src/brf2ebrf/common/block_detectors.py
+++ src/brf2ebrf/common/block_detectors.py
@@ -26,7 +26,7 @@ def detect_pre(
 
 def create_cell_heading(indent: int, tag_name: str) -> Detector:
     """Creates a detector for a heading indented by the specified amount."""
-    heading_re = re.compile(f"\u2800{{{indent}}}([\u2801-\u28ff][\u2800-\u28ff]*)[\n]+")
+    heading_re = re.compile(f"\u2800{{{indent}}}([\u2801-\u28ff][\u2800-\u28ff]*)\n+")
 
     def detect_cell_heading(
             text: str, cursor: int, state: DetectionState, output_text: str
@@ -50,7 +50,7 @@ def create_centered_detector(
         cells_per_line: int, min_indent: int, tag_name: str
 ) -> Detector:
     """Creates a detector for detecting centered text."""
-    heading_re = re.compile(f"(\u2800{{{min_indent},}})([\u2801-\u28ff][\u2800-\u28ff]*)[\n]+", )
+    heading_re = re.compile(f"(\u2800{{{min_indent},}})([\u2801-\u28ff][\u2800-\u28ff]*)\n+", )
 
     def detect_centered(
             text: str, cursor: int, state: DetectionState, output_text: str
@@ -86,9 +86,9 @@ _PROCESSING_INSTRUCTION_RE = f"{_PRINT_PAGE_RE}|{_BRAILLE_PAGE_RE}|{_RUNNING_HEA
 
 def _create_indented_block_finder(first_line_indent: int, run_over: int) -> Callable[[str, int], (str | None, int)]:
     _first_line_re = re.compile(
-        f"^\u2800{{{first_line_indent}}}({_PROCESSING_INSTRUCTION_RE}|[\u2801-\u28ff][\u2800-\u28ff]*)(?:[\n]|{_BLANK_LINE_RE})")
+        f"^\u2800{{{first_line_indent}}}({_PROCESSING_INSTRUCTION_RE}|[\u2801-\u28ff][\u2800-\u28ff]*)(?:\n|{_BLANK_LINE_RE})")
     _run_over_re = re.compile(
-        f"\u2800{{{run_over}}}({_PROCESSING_INSTRUCTION_RE}|[\u2801-\u28ff][\u2800-\u28ff]*)(?:[\n]|{_BLANK_LINE_RE})")
+        f"\u2800{{{run_over}}}({_PROCESSING_INSTRUCTION_RE}|[\u2801-\u28ff][\u2800-\u28ff]*)(?:\n|{_BLANK_LINE_RE})")
 
     def find_paragraph_braille(text: str, cursor: int) -> (str | None, int):
         if line := _first_line_re.match(
@@ -137,9 +137,9 @@ def create_paragraph_detector(first_line_indent: int, run_over: int,
 def create_list_detector(first_line_indent: int, run_over: int) -> Detector:
     """Creates a detector for finding lists with the specified first line indent and run over."""
     first_line_re = re.compile(
-        f"^\u2800{{{first_line_indent}}}({_PROCESSING_INSTRUCTION_RE}|[\u2801-\u28ff][\u2800-\u28ff]*)(?:[\n]|{_BLANK_LINE_RE})")
+        f"^\u2800{{{first_line_indent}}}({_PROCESSING_INSTRUCTION_RE}|[\u2801-\u28ff][\u2800-\u28ff]*)(?:\n|{_BLANK_LINE_RE})")
     run_over_re = re.compile(
-        f"\u2800{{{run_over},}}({_PROCESSING_INSTRUCTION_RE}|[\u2801-\u28ff][\u2800-\u28ff]*)(?:[\n]|{_BLANK_LINE_RE})")
+        f"\u2800{{{run_over},}}({_PROCESSING_INSTRUCTION_RE}|[\u2801-\u28ff][\u2800-\u28ff]*)(?:\n|{_BLANK_LINE_RE})")
 
     def detect_list(
             text: str, cursor: int, state: DetectionState, output_text: str
