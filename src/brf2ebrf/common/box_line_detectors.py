f2e68c41f40527440c2b9231bce941da4cf6379b
Ken Perry <kperry@aph.org>
2024-03-22 14:10:02 -0400

    Box line code clean up

--- src/brf2ebrf/common/box_line_detectors.py
+++ src/brf2ebrf/common/box_line_detectors.py
@@ -6,24 +6,31 @@
 
 """Detectors for Box lines"""
 import re
-from collections.abc import Iterable
-import logging
 
-from brf2ebrf.parser import DetectionState, DetectionResult, Detector
+from brf2ebrf.parser import DetectionState, DetectionResult
 
 # Define the regular expression patterns
-_ENCLOSING_RE = re.compile(r"(?:([\u2801-\u28ff]+?)\u2800)*?(\u283f{10,})(.+?)(\u283f{10,})",re.DOTALL)
-_BOX_RE = re.compile(r"(?:([\u2801-\u28ff]+?)\u2800)*?(\u2836{10,})(.+?)(\u281b{10,})",re.DOTALL)
+_ENCLOSING_RE = re.compile(
+    r"(?:([\u2801-\u28ff]+?)\u2800)*?(\u283f{10,})(.+?)(\u283f{10,})", re.DOTALL
+)
+_BOX_RE = re.compile(
+    r"(?:([\u2801-\u28ff]+?)\u2800)*?(\u2836{10,})(.+?)(\u281b{10,})", re.DOTALL
+)
 _BOX_LINES_PROCESSING_INSTRUCTION_RE = re.compile(R"<\?box ([\u2800-\u28ff]+)\?>")
 
+
 def _convert_groups(match):
+    """regular expression group substitution function"""
     if match.group(1):
-        return f'<div screen_type="<?box {match.group(1)}?>" type="<?box {match.group(2)[0]}?>">{match.group(3)}</div>'
-    return  f'<div type="<?box {match.group(2)[0]}?>">{match.group(3)}</div>'
+        return (
+            f'<div screen_type="<?box {match.group(1)}?>" type="<?box '
+            +f'{match.group(2)[0]}?>">{match.group(3)}</div>'
+        )
+    return f'<div type="<?box {match.group(2)[0]}?>">{match.group(3)}</div>'
 
 
 def convert_box_lines(
-        text: str, cursor: int, state: DetectionState, output_text: str
+    text: str, _: int, state: DetectionState, output_text: str
 ) -> DetectionResult | None:
     """
     converts all box and screen material to their div equivlant or returns None if not a box line
@@ -33,13 +40,26 @@ def convert_box_lines(
 
     Returns:
     - The changed file worth of text in a detection result
-    
+
     """
-    return DetectionResult(len(text), state, 1.0, output_text + _ENCLOSING_RE.sub(_convert_groups,_BOX_RE.sub(_convert_groups,text)))
+    return DetectionResult(
+        len(text),
+        state,
+        1.0,
+        output_text
+        + _ENCLOSING_RE.sub(_convert_groups, _BOX_RE.sub(_convert_groups, text)),
+    )
 
 
-def remove_box_lines_processing_instructions(text: str, cursor: int, state: DetectionState, output_text: str) -> DetectionResult | None:
+def remove_box_lines_processing_instructions(
+    text: str, _: int, state: DetectionState, output_text: str
+) -> DetectionResult | None:
     """Remove box processing instructions. This was to stop the pre problem."""
-    
-    return DetectionResult(len(text), state, 1.0, output_text + _BOX_LINES_PROCESSING_INSTRUCTION_RE.sub(lambda m: m.group(1),text))
 
+    return DetectionResult(
+        len(text),
+        state,
+        1.0,
+        output_text
+        + _BOX_LINES_PROCESSING_INSTRUCTION_RE.sub(lambda m: m.group(1), text),
+    )
