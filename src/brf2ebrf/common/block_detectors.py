c88032b763e3b05977f65a312fabde617574eb93
Michael Whapples <software@mdw.onl>
2024-04-11 11:07:24 +0100

    Redo regex for processing instructions relating to page boundaries.

--- src/brf2ebrf/common/block_detectors.py
+++ src/brf2ebrf/common/block_detectors.py
@@ -81,14 +81,14 @@ _PRINT_PAGE_RE = "(?:<\\?print-page[ \u2800-\u28ff]*?\\?>)"
 _RUNNING_HEAD_RE = "(?:<\\?running-head[ \u2800-\u28ff]*\\?>)"
 _BRAILLE_PAGE_RE = "(?:<\\?braille-page[ \u2800-\u28ff]*\\?>)"
 _BLANK_LINE_RE = "(?:<\\?blank-line\\?>)"
-_PROCESSING_INSTRUCTION_RE = f"(?:\n*(?:{_BRAILLE_PAGE_RE}{_PRINT_PAGE_RE}{{0,1}}{_RUNNING_HEAD_RE}{{0,1}})|{_PRINT_PAGE_RE}\n*)"
+_PROCESSING_INSTRUCTION_RE = f"(?:(?:(?:{_BLANK_LINE_RE}\n)?{_BRAILLE_PAGE_RE}\n{_PRINT_PAGE_RE}\n(?:{_RUNNING_HEAD_RE}\n)?(?:{_BLANK_LINE_RE}\n)?)|?:{_BRAILLE_PAGE_RE}\n(?:{_RUNNING_HEAD_RE}\n)?)|(?:{_PRINT_PAGE_RE}\n))"
 
 
 def _create_indented_block_finder(first_line_indent: int, run_over: int) -> Callable[[str, int], (str | None, int)]:
     _first_line_re = re.compile(
-        f" {{{first_line_indent}}}([\u2801-\u28ff][ \u2800-\u28ff]*(?:\n{_BLANK_LINE_RE}?))")
+        f" {{{first_line_indent}}}([\u2801-\u28ff][ \u2800-\u28ff]*\n)")
     _run_over_re = re.compile(
-        f"({_PROCESSING_INSTRUCTION_RE}*) {{{run_over}}}([\u2801-\u28ff][ \u2800-\u28ff]*(?:\n{_BLANK_LINE_RE}?))")
+        f"({_PROCESSING_INSTRUCTION_RE}*) {{{run_over}}}([\u2801-\u28ff][ \u2800-\u28ff]*\n)")
 
     def find_paragraph_braille(text: str, cursor: int) -> (str | None, int):
         if line := _first_line_re.match(
