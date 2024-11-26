bf3d6b510645f171679c98b1dcbc0cebd1dad744
Michael Whapples <software@mdw.onl>
2024-11-26 19:39:44 +0000

    Add initial page list nav element.

--- src/brf2ebrl/utils/ebrl.py
+++ src/brf2ebrl/utils/ebrl.py
@@ -4,8 +4,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """Utilities for working with eBraille format."""
+from collections.abc import Iterable
+from dataclasses import dataclass
+
 import lxml.html
-from lxml.html.builder import HTML, HEAD, BODY, ATTR, META, LINK, TITLE
+from lxml.html.builder import HTML, HEAD, BODY, ATTR, META, LINK, TITLE, H2, OL, LI, A
 
 
 def XMLNS(uri: str, prefix=None) -> dict[str, str]:
@@ -15,7 +18,21 @@ def XMLNS(uri: str, prefix=None) -> dict[str, str]:
 def XML_LANG(lang: str) -> dict[str, str]:
     return {"xml:lang": lang}
 
-def create_navigation_html(title: str = "-", opf_name: str = "package.opf") -> str:
+def EPUB_TYPE(epub_type: str) -> dict[str, str]:
+    return {"epub:type": epub_type}
+
+def ARIA_LABEL(label: str) -> dict[str, str]:
+    return {"aria-label": label}
+
+NAV = lxml.html.builder.E.nav
+
+@dataclass(frozen=True)
+class PageRef:
+    href: str
+    title: str
+    page_num_braille: str
+
+def create_navigation_html(title: str = "-", page_refs: Iterable[PageRef] = (), opf_name: str = "package.opf") -> str:
     root = HTML(
         XMLNS("http://www.w3.org/1999/xhtml"),
         XMLNS("http://www.idpf.org/2007/ops", prefix="epub"),
@@ -32,6 +49,17 @@ def create_navigation_html(title: str = "-", opf_name: str = "package.opf") -> s
                 type="application/oebps-package+xml"
             ),
         ),
-        BODY()
+        BODY(
+            NAV(
+                EPUB_TYPE("page-list"),
+                ARIA_LABEL("Page list"),
+                ATTR(role="doc-pagelist"),
+                ATTR(hidden=""),
+                H2("⠠⠇⠊⠌ ⠷ ⠏⠁⠛⠑⠎"),
+                OL(
+                    *[LI(A(ATTR(href=r.href), r.page_num_braille)) for r in page_refs]
+                )
+            )
+        )
     )
     return lxml.html.tostring(root, pretty_print=True, method="xml", doctype="<!DOCTYPE html>")
