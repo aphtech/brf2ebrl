fc0d30b961fc2e6249d52ae592a65cd2db2714ea
Michael Whapples <software@mdw.onl>
2025-02-12 15:09:57 +0000

    Refactor emphasis tests to make them parameterised.

--- tests/test_emphasis_detectors.py
+++ tests/test_emphasis_detectors.py
@@ -3,66 +3,27 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import pytest
 
 from brf2ebrl.common.emphasis_detectors import tag_emphasis
 
 
-def test_convert_phrase_emphasis():
-    #simple phrase tests
-    brf = "\u2818\u2836\u2801\u2803\u2809\u2800\u2801\u2803\u2809\u2800\u2801\u2803\u2809\u2800\u2801\u2803\u2809\u2819\u2818\u2804"
-    expected_brf = '<strong>⠘⠶⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠙⠘⠄</strong>'
-    actual = tag_emphasis(brf)
-    expected = expected_brf
-    assert actual == expected
-    brf = '⠘⠶⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠙⠘⠄⠀⠁⠃⠉'
-    expected_brf = '<strong>⠘⠶⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠙⠘⠄</strong>⠀⠁⠃⠉'
-    actual = tag_emphasis(brf)
-    expected = expected_brf
-    assert actual == expected
-    brf = '⠘⠶⠐⠼⠶⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠙⠐⠼⠄⠘⠄⠀⠁⠃⠉'
-    expected_brf = '<strong>⠘⠶<em class="trans4">⠐⠼⠶⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠙⠐⠼⠄</em>⠘⠄</strong>⠀⠁⠃⠉'
-    actual = tag_emphasis(brf)
-    expected = expected_brf
-    assert actual == expected
-        
-def test_convert_word_emphasis():
-    #simple wordtests
-    brf = "⠨⠂⠁⠃⠉⠀"
-    expected_brf = '<em>⠨⠂⠁⠃⠉</em>⠀'
-    actual = tag_emphasis(brf)
-    expected = expected_brf
-    assert actual == expected
-    brf = '⠐⠼⠂⠁⠃⠉⠀⠁⠃⠉⠀⠐⠼⠂⠸⠂⠁⠃⠉⠀⠁⠃⠉⠙⠀⠁⠃⠉'
-    expected_brf = '<em class="trans4">⠐⠼⠂⠁⠃⠉</em>⠀⠁⠃⠉⠀<em class="trans4">⠐⠼⠂<em class="underline">⠸⠂⠁⠃⠉</em></em>⠀⠁⠃⠉⠙⠀⠁⠃⠉'
-    actual = tag_emphasis(brf)
-    expected = expected_brf
-    assert actual == expected
-    brf = '⠨⠂⠁⠃⠉⠨⠄⠄⠀⠁'
-    expected_brf = '<em>⠨⠂⠁⠃⠉⠨⠄</em>⠄⠀⠁'
-    actual = tag_emphasis(brf)
-    expected = expected_brf
-    assert actual == expected
-                
-def test_convert_letter_emphasis():
+@pytest.mark.parametrize("text,expected_text", [
+    # Simple phrase tests
+    ("\u2818\u2836\u2801\u2803\u2809\u2800\u2801\u2803\u2809\u2800\u2801\u2803\u2809\u2800\u2801\u2803\u2809\u2819\u2818\u2804", '<strong>⠘⠶⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠙⠘⠄</strong>'),
+    ('⠘⠶⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠙⠘⠄⠀⠁⠃⠉', '<strong>⠘⠶⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠙⠘⠄</strong>⠀⠁⠃⠉'),
+    ('⠘⠶⠐⠼⠶⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠙⠐⠼⠄⠘⠄⠀⠁⠃⠉', '<strong>⠘⠶<em class="trans4">⠐⠼⠶⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠀⠁⠃⠉⠙⠐⠼⠄</em>⠘⠄</strong>⠀⠁⠃⠉'),
+    # Simple word tests
+    ("⠨⠂⠁⠃⠉⠀", '<em>⠨⠂⠁⠃⠉</em>⠀'),
+    ('⠐⠼⠂⠁⠃⠉⠀⠁⠃⠉⠀⠐⠼⠂⠸⠂⠁⠃⠉⠀⠁⠃⠉⠙⠀⠁⠃⠉', '<em class="trans4">⠐⠼⠂⠁⠃⠉</em>⠀⠁⠃⠉⠀<em class="trans4">⠐⠼⠂<em class="underline">⠸⠂⠁⠃⠉</em></em>⠀⠁⠃⠉⠙⠀⠁⠃⠉'),
+    ('⠨⠂⠁⠃⠉⠨⠄⠄⠀⠁', '<em>⠨⠂⠁⠃⠉⠨⠄</em>⠄⠀⠁'),
     #simple letter tests
-    brf = "⠘⠆⠁⠃⠉"
-    expected_brf = '<strong>⠘⠆⠁</strong>⠃⠉'
-    actual = tag_emphasis(brf)
-    expected = expected_brf
-    assert actual == expected
-    brf = '⠘⠆⠠⠘⠺⠀'
-    expected_brf = '<strong>⠘⠆⠠⠘⠺</strong>⠀'
-    actual = tag_emphasis(brf)
-    expected = expected_brf
-    assert actual == expected
-    brf = '⠈⠆⠨⠆⠁⠃⠸⠆⠉'
-    expected_brf = '<em>⠨⠆<em class="script">⠈⠆⠁</em></em>⠃<em class="underline">⠸⠆⠉</em>'
-    actual = tag_emphasis(brf)
-    expected = expected_brf
-    assert actual == expected
-
-def test_nested_emphasis():
-    brf = "<h2>⠠⠩⠕⠑⠀⠠⠁⠀⠊⠎⠀⠁⠃⠀⠼⠃⠀⠔⠡⠑⠎⠀⠇⠰⠛⠲⠀⠠⠩⠕⠑⠀⠰⠠⠃⠀⠊⠎⠀⠁⠃⠀⠼⠙⠀⠔⠡⠑⠎⠀⠇⠰⠛⠲⠀⠠⠩⠕⠑⠀⠠⠁⠀⠊⠎⠀⠼⠃⠀⠔⠡⠑⠎⠀⠈⠼⠂⠘⠂⠩⠕⠗⠞⠻⠀⠘⠂⠇⠰⠛⠻</h2>"
-    actual = tag_emphasis(brf)
-    expected = "<h2>⠠⠩⠕⠑⠀⠠⠁⠀⠊⠎⠀⠁⠃⠀⠼⠃⠀⠔⠡⠑⠎⠀⠇⠰⠛⠲⠀⠠⠩⠕⠑⠀⠰⠠⠃⠀⠊⠎⠀⠁⠃⠀⠼⠙⠀⠔⠡⠑⠎⠀⠇⠰⠛⠲⠀⠠⠩⠕⠑⠀⠠⠁⠀⠊⠎⠀⠼⠃⠀⠔⠡⠑⠎⠀<em class=\"trans1\">⠈⠼⠂<strong>⠘⠂⠩⠕⠗⠞⠻</strong></em>⠀<strong>⠘⠂⠇⠰⠛⠻</strong></h2>"
-    assert actual == expected
+    ("⠘⠆⠁⠃⠉", '<strong>⠘⠆⠁</strong>⠃⠉'),
+    ('⠘⠆⠠⠘⠺⠀', '<strong>⠘⠆⠠⠘⠺</strong>⠀'),
+    ('⠈⠆⠨⠆⠁⠃⠸⠆⠉', '<em>⠨⠆<em class="script">⠈⠆⠁</em></em>⠃<em class="underline">⠸⠆⠉</em>'),
+    # Nesting tests
+    ("<h2>⠠⠩⠕⠑⠀⠠⠁⠀⠊⠎⠀⠁⠃⠀⠼⠃⠀⠔⠡⠑⠎⠀⠇⠰⠛⠲⠀⠠⠩⠕⠑⠀⠰⠠⠃⠀⠊⠎⠀⠁⠃⠀⠼⠙⠀⠔⠡⠑⠎⠀⠇⠰⠛⠲⠀⠠⠩⠕⠑⠀⠠⠁⠀⠊⠎⠀⠼⠃⠀⠔⠡⠑⠎⠀⠈⠼⠂⠘⠂⠩⠕⠗⠞⠻⠀⠘⠂⠇⠰⠛⠻</h2>", "<h2>⠠⠩⠕⠑⠀⠠⠁⠀⠊⠎⠀⠁⠃⠀⠼⠃⠀⠔⠡⠑⠎⠀⠇⠰⠛⠲⠀⠠⠩⠕⠑⠀⠰⠠⠃⠀⠊⠎⠀⠁⠃⠀⠼⠙⠀⠔⠡⠑⠎⠀⠇⠰⠛⠲⠀⠠⠩⠕⠑⠀⠠⠁⠀⠊⠎⠀⠼⠃⠀⠔⠡⠑⠎⠀<em class=\"trans1\">⠈⠼⠂<strong>⠘⠂⠩⠕⠗⠞⠻</strong></em>⠀<strong>⠘⠂⠇⠰⠛⠻</strong></h2>"),
+])
+def test_detect_emphasis(text, expected_text):
+    actual = tag_emphasis(text)
+    assert actual == expected_text
