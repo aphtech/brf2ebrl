3292903f4445cf1847b653030c807b83606f9622
Michael Whapples <software@mdw.onl>
2025-02-19 16:10:04 +0000

    For now have date stored as a string.

--- src/brf2ebrl/utils/metadata.py
+++ src/brf2ebrl/utils/metadata.py
@@ -63,7 +63,7 @@ class Language(MetadataItem):
 
 DEFAULT_METADATA = [Creator("-"),
           Format("eBraille 1.0"),
-          Date(datetime.date.today()),
+          Date(datetime.date.today().isoformat()),
           Identifier(str(uuid4())),
           Language("en-Brai"),
           Title("-")]
