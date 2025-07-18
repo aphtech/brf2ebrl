3f1a7e33a09b1798d8b3afa98772477f874ae6b5
Michael Whapples <mwhapples@aph.org>
2025-07-18 12:11:55 +0100

    Revert "Go back to the original attempt, I think the issue may be in packaging."
    This reverts commit d45bef14429d91177912f3ef3862f43f7e21a6de.

--- src/brf2ebrl/utils/__init__.py
+++ src/brf2ebrl/utils/__init__.py
@@ -5,6 +5,8 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """Useful utility functions"""
 import re
+from collections.abc import Iterable
+from importlib.resources.abc import Traversable
 
 _TAG_NAME_PATTERN = "[_a-zA-Z][-_.a-zA-Z0-9]*"
 _ELEMENT_TAG_RE = re.compile(
@@ -29,3 +31,6 @@ def find_end_of_element(text: str, start: int = 0) -> int:
         if len(tags) == 0:
             break
     return cursor if len(tags) == 0 else -1
+
+def list_sub_paths(path: Traversable) -> Iterable[tuple[list[str], Traversable]]:
+    return [([path.name], path)] if not path.is_dir() else [([path.name] + l, p) for i in path.iterdir() for l, p in list_sub_paths(i)]
