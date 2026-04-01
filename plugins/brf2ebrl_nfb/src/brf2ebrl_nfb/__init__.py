3b752e6eda36049bed5eb09f841b8166fba0b16f
Ken Perry <kperry@aph.org>
2026-04-01 17:29:52 -0400

    updated NFB part of plugin

--- plugins/brf2ebrl_nfb/src/brf2ebrl_nfb/__init__.py
+++ plugins/brf2ebrl_nfb/src/brf2ebrl_nfb/__init__.py
@@ -109,9 +109,8 @@ def create_brf2ebrl_parser(
                     create_centered_detector(page_layout.cells_per_line, 3, "h1"),
                     create_cell_heading(6, "h3"),
                     create_cell_heading(4, "h2"),
-                    create_paragraph_detector(6, 4,page_layout.cells_per_line, tn_indicators_block_matcher, confidence=0.95),
-                    create_paragraph_detector(2, 0,page_layout.cells_per_line, confidence=0.9),
-                    create_paragraph_detector(0, 0,page_layout.cells_per_line ,bp_indicators_block_matcher, confidence=0.9),
+                    create_paragraph_detector(6, 4,page_layout, tn_indicators_block_matcher, confidence=0.95),
+                    create_paragraph_detector(2, 0,page_layout, confidence=0.9),
                     create_toc_detector(page_layout.cells_per_line),
                     create_list_detector(page_layout.cells_per_line),
                     create_table_detector(),  # might add arguments later
